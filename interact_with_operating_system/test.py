dc4d8f5e285974652ff3d4c25368be2e0fa59b8c
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-01-17 16:43:21 +0700

    advanced regex

--- interact_with_operating_system/test.py
+++ interact_with_operating_system/test.py
@@ -1,6 +1,9 @@
 import csv
 import os
 import re
+
+
+
 # import datetime
 #
 # parent_dir = os.path.abspath(os.path.join(os.getcwd(),".."))
@@ -30,12 +33,37 @@ import re
 # print(re.findall(r"cats|dogs", "I like both dogs and cats."))
 
 
-def check_character_groups(text):
-  result = re.search(r"\w+ ", text)
-  return result is not None
+# def check_character_groups(text):
+#   result = re.search(r"\w+ ", text)
+#   return result is not None
+#
+#
+# print(check_character_groups("One")) # False
+# print(check_character_groups("123  Ready Set GO")) # True
+# print(check_character_groups("username user_01")) # True
+# print(check_character_groups("shopping_list: milk, bread, eggs.")) # False
 
 
-print(check_character_groups("One")) # False
-print(check_character_groups("123  Ready Set GO")) # True
-print(check_character_groups("username user_01")) # True
-print(check_character_groups("shopping_list: milk, bread, eggs.")) # False
+
+# def check_sentence(text):
+#     result = re.search(r"^[A-Z].*[.!?]$", text)
+#     return result != None
+#
+# # Test cases
+# print(check_sentence("Is this is a sentence?"))  # True
+# print(check_sentence("is this is a sentence?"))  # False
+# print(check_sentence("Hello"))  # False
+# print(check_sentence("1-2-3-GO!"))  # False
+# print(check_sentence("A star is born."))  # True
+
+# result = re.search(r"\d+","this is my number: 123-234-123-123")
+# print(result)
+def rearrange_name(name):
+  result = re.search(r"^(\w*) (\w.*)$", name)
+  if result is None:
+    return name
+  return "{} {}".format(result[2], result[1])
+name=rearrange_name("Nguyen Ngoc Hai")
+full_name = rearrange_name("Lucas Daniel")
+print(name)
+print(full_name)
